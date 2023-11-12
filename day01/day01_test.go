cf81eaacb75d241e077aa9277cff9e9416eef801
Joe Nace <jwnace@gmail.com>
2023-11-12 02:53:35 -0500

    replaced tabs with spaces

--- day01/day01_test.go
+++ day01/day01_test.go
@@ -3,46 +3,46 @@ package day01
 import "testing"
 
 func TestPart1Example(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected int
-	}{
-		{"(())", 0},
-		{"()()", 0},
-		{"(((", 3},
-		{"(()(()(", 3},
-		{"))(((((", 3},
-		{"())", -1},
-		{"))(", -1},
-		{")))", -3},
-		{")())())", -3},
-	}
+    tests := []struct {
+        input    string
+        expected int
+    }{
+        {"(())", 0},
+        {"()()", 0},
+        {"(((", 3},
+        {"(()(()(", 3},
+        {"))(((((", 3},
+        {"())", -1},
+        {"))(", -1},
+        {")))", -3},
+        {")())())", -3},
+    }
 
-	for _, test := range tests {
-		t.Run(test.input, func(t *testing.T) {
-			actual := Solve1(test.input)
+    for _, test := range tests {
+        t.Run(test.input, func(t *testing.T) {
+            actual := Solve1(test.input)
 
-			if actual != test.expected {
-				t.Errorf("Part1(%s) = %d, expected %d", test.input, actual, test.expected)
-			}
-		})
-	}
+            if actual != test.expected {
+                t.Errorf("Part1(%s) = %d, expected %d", test.input, actual, test.expected)
+            }
+        })
+    }
 }
 
 func TestPart1Solution(t *testing.T) {
-	expected := 280
-	actual := Part1()
+    expected := 280
+    actual := Part1()
 
-	if actual != expected {
-		t.Errorf("Part1() = %d, expected %d", actual, expected)
-	}
+    if actual != expected {
+        t.Errorf("Part1() = %d, expected %d", actual, expected)
+    }
 }
 
 func TestPart2Solution(t *testing.T) {
-	expected := 1797
-	actual := Part2()
+    expected := 1797
+    actual := Part2()
 
-	if actual != expected {
-		t.Errorf("Part2() = %d, expected %d", actual, expected)
-	}
+    if actual != expected {
+        t.Errorf("Part2() = %d, expected %d", actual, expected)
+    }
 }
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"joenace.com/aoc-2015-go/day01"
+    "fmt"
+    "joenace.com/aoc-2015-go/day01"
 )
 
 func main() {
-	fmt.Printf("Day 01, Part 1: %v\n", day01.Part1())
-	fmt.Printf("Day 01, Part 2: %v\n", day01.Part2())
+    fmt.Printf("Day 01, Part 1: %v\n", day01.Part1())
+    fmt.Printf("Day 01, Part 2: %v\n", day01.Part2())
 }
