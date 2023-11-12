d05fd72620b8646c31dbec39a6668dc216eaf36e
Joe Nace <jwnace@gmail.com>
2023-11-11 23:39:35 -0500

    added tests and solutions for day 1

--- day01/day01.go
+++ day01/day01.go
@@ -1,9 +1,61 @@
 package day01
 
+import (
+	"os"
+	"path"
+	"runtime"
+)
+
+func loadInput() string {
+	_, file, _, _ := runtime.Caller(0)
+	dir := path.Dir(file)
+	input, err := os.ReadFile(dir + "/input.txt")
+
+	if err != nil {
+		return ""
+	}
+
+	return string(input)
+}
+
 func Part1() int {
-	return 1
+	input := loadInput()
+	return Solve1(input)
 }
 
 func Part2() int {
-	return 2
+	input := loadInput()
+	return Solve2(input)
+}
+
+func Solve1(input string) int {
+	sum := 0
+
+	for _, value := range input {
+		if value == '(' {
+			sum++
+		} else if value == ')' {
+			sum--
+		}
+	}
+
+	return sum
+}
+
+func Solve2(input string) int {
+	sum := 0
+
+	for i, value := range input {
+		if value == '(' {
+			sum++
+		} else if value == ')' {
+			sum--
+
+			if sum < 0 {
+				return i + 1
+			}
+		}
+	}
+
+	return 0
 }
