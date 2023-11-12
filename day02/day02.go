8e602c4afa171306e9bfe0a9989af989b3f55296
Joe Nace <jwnace@gmail.com>
2023-11-12 13:13:11 -0500

    added boilerplate for day 2

---
+++ day02/day02.go
@@ -0,0 +1,19 @@
+package day02
+
+func Part1() int {
+	//input := loadInput()
+	return Solve1("input")
+}
+
+func Part2() int {
+	//input := loadInput()
+	return Solve2("input")
+}
+
+func Solve1(input string) int {
+	return 1
+}
+
+func Solve2(input string) int {
+	return 2
+}
---
+++ day02/day02_test.go
@@ -0,0 +1,40 @@
+package day02
+
+import "testing"
+
+func TestPart1Example(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		// TODO: add test cases
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			actual := Solve1(test.input)
+
+			if actual != test.expected {
+				t.Errorf("Part1(%s) = %d, expected %d", test.input, actual, test.expected)
+			}
+		})
+	}
+}
+
+func TestPart1Solution(t *testing.T) {
+	expected := 0
+	actual := Part1()
+
+	if actual != expected {
+		t.Errorf("Part1() = %d, expected %d", actual, expected)
+	}
+}
+
+func TestPart2Solution(t *testing.T) {
+	expected := 0
+	actual := Part2()
+
+	if actual != expected {
+		t.Errorf("Part2() = %d, expected %d", actual, expected)
+	}
+}
