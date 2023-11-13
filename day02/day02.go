ec0dba0495421af7beb0a98192d2b669ee2a6fae
Joe Nace <jwnace@gmail.com>
2023-11-13 17:20:33 -0500

    added solution for day 2

--- day02/day02.go
+++ day02/day02.go
@@ -1,6 +1,10 @@
 package day02
 
-import "joenace.com/aoc-2015-go/helpers"
+import (
+	"joenace.com/aoc-2015-go/helpers"
+	"strconv"
+	"strings"
+)
 
 func Part1() int {
 	input := helpers.ReadInput()
@@ -13,9 +17,69 @@ func Part2() int {
 }
 
 func Solve1(input string) int {
-	return 1
+	sum := 0
+
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		parts := strings.Split(line, "x")
+
+		l, err := strconv.Atoi(parts[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		w, err := strconv.Atoi(parts[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		h, err := strconv.Atoi(parts[2])
+
+		if err != nil {
+			panic(err)
+		}
+
+		sum += getWrappingPaperRequired(l, w, h)
+	}
+
+	return sum
 }
 
 func Solve2(input string) int {
-	return 2
+	sum := 0
+
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		parts := strings.Split(line, "x")
+
+		l, err := strconv.Atoi(parts[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		w, err := strconv.Atoi(parts[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		h, err := strconv.Atoi(parts[2])
+
+		if err != nil {
+			panic(err)
+		}
+
+		sum += getRibbonRequired(l, w, h)
+	}
+
+	return sum
+}
+
+func getWrappingPaperRequired(l int, w int, h int) int {
+	return 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
+}
+
+func getRibbonRequired(l int, w int, h int) int {
+	return 2*min(l+w, w+h, h+l) + l*w*h
 }
