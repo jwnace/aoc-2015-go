2002151990246bda2b81699c9c268c63827d1274
Joe Nace <jwnace@gmail.com>
2023-11-13 22:16:24 -0500

    updated tests for part 1 and 2 including examples

--- day03/day03_test.go
+++ day03/day03_test.go
@@ -2,8 +2,29 @@ package day03
 
 import "testing"
 
+func TestPart1Example(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{">", 2},
+		{"^>v<", 4},
+		{"^v^v^v^v^v", 2},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			actual := solve1(test.input)
+
+			if actual != test.expected {
+				t.Errorf("Part1(%s) = %d, expected %d", test.input, actual, test.expected)
+			}
+		})
+	}
+}
+
 func TestPart1Solution(t *testing.T) {
-	expected := 0
+	expected := 2081
 	actual := Part1()
 
 	if actual != expected {
@@ -11,8 +32,29 @@ func TestPart1Solution(t *testing.T) {
 	}
 }
 
+func TestPart2Example(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{
+		{"^v", 3},
+		{"^>v<", 3},
+		{"^v^v^v^v^v", 11},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			actual := solve1(test.input)
+
+			if actual != test.expected {
+				t.Errorf("Part1(%s) = %d, expected %d", test.input, actual, test.expected)
+			}
+		})
+	}
+}
+
 func TestPart2Solution(t *testing.T) {
-	expected := 0
+	expected := 2341
 	actual := Part2()
 
 	if actual != expected {
